499d72eb588a4bc092426bc3fb94e2f93e4c4d79
Ville Niemi <vilsuxn@gmail.com>
2023-04-09 20:12:07 +0300

    added mui styling to current weather box

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import CurrentWeatherBox from './components/CurrentWeatherBox';
+import Container from '@mui/material/Container';
 
 function App() {
 
   return (
-    <>
+    <Container maxWidth="sm">
       <CurrentWeatherBox />
-    </>
+    </Container>
   );
 }
 
--- src/components/CurrentWeatherBox.tsx
+++ src/components/CurrentWeatherBox.tsx
@@ -1,20 +1,47 @@
 import { useEffect, useState } from "react"
 import { getCurrentWeather } from "../services/services"
+import Grid2 from '@mui/material/Unstable_Grid2';
+import Box from '@mui/material/Box';
+import Paper from "@mui/material/Paper";
+import { styled } from "@mui/material/styles";
 
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
 
 const CurrentWeatherBox = () => {
-    const[weatherData, setWeatherData] = useState<any | null>(null);
+    const[weather, setWeather] = useState<any | null>(null);
 
     useEffect(() => {
-        getCurrentWeather(10.99, 44.34)
-        .then(res => setWeatherData(res))
-    })
+        getCurrentWeather(62.203075, 23.176253)
+        .then(res => setWeather(res))
+    }, [])
 
-    return (
-        <>
-            {weatherData != null && <p>Temperature is {weatherData.main.temp} Kelvin</p>}
-        </>
+    console.log(weather);
+    return ( 
+        <Box sx={{ flexGrow: 1 }}>
+        {weather != null &&
+            <Grid2 container spacing={1}>
+            <Grid2 xs={6}>
+                <Item>{weather.name} {weather.weather[0].description}</Item>
+            </Grid2>
+            <Grid2 xs={6}>
+                <Item>{weather.weather[0].icon} {weather.main.temp} C</Item>
+            </Grid2>
+            <Grid2 xs={6}>
+                <Item>{weather.dt}</Item>
+            </Grid2>
+            <Grid2 xs={6}>
+                <Item>{weather.wind.speed} - {weather.main.humidity} - {weather.rain}</Item>
+            </Grid2>
+            </Grid2>
+        }
+      </Box>
     )
 }
-
+//{weatherData != null && <p>Temperature is {weatherData.main.temp} Kelvin</p>}
 export default CurrentWeatherBox;
