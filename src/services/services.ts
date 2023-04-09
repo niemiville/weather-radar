61dfdd04b3625e9b4e7d42e2473d1260b7f043f1
Ville Niemi <vilsuxn@gmail.com>
2023-04-09 20:11:36 +0300

    changed request to use metric units parameter

--- src/services/services.ts
+++ src/services/services.ts
@@ -5,7 +5,7 @@ axios.defaults.baseURL = 'https://api.openweathermap.org/data/2.5';
 const appid = process.env.REACT_APP_WEATHER_API_KEY;
 
 export const getCurrentWeather = (lat: number, lon: number) => {
-    return axios.get('/weather', { params: { lat, lon, appid } })
+    return axios.get('/weather', { params: { lat, lon, appid, units: 'metric' } })
     .then(res => res.data)
     .catch(err => console.log(err.response.data))
 }
